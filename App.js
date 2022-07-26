13c83c3bb9baf77a25877d90049f6d2c8ca79fdb
IDStack <idstack@outlook.co.id>
2022-07-26 22:18:58 +0700

    mendapatkan data movie dari api dengan axios

--- App.js
+++ App.js
@@ -6,10 +6,19 @@
  * @flow strict-local
  */
 
+import axios from 'axios';
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+const getPopMovies = async () => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/popular?api_key=778306490922af732ee8ce5aeb1ef02e`,
+  );
+  console.log(JSON.stringify(response.data.results[0], null, 2));
+};
+
 const App = () => {
+  getPopMovies();
   return (
     <>
       <View style={styles.container}>
