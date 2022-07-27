e311e20612663e4cfc2bd4580ff68e9c7dabae25
IDStack <idstack@outlook.co.id>
2022-07-27 13:50:33 +0700

    menampilkan poster dengan image slider

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,33 @@
 /* eslint-disable */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {SliderBox} from 'react-native-image-slider-box';
+import {getPopularMovies} from '../services/request';
 
 const Home = () => {
+  const [movieImages, setmovieImages] = useState([]);
+
+  useEffect(() => {
+    getPopularMovies()
+      .then((movies) => {
+        let images = [];
+        for (let movie = 0; movie < movies.length; movie++) {
+          images.push(
+            `https://image.tmdb.org/t/p/w500${movies[movie]['poster_path']}`,
+          );
+        }
+        setmovieImages(images);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <View style={styles.container}>
+        <SliderBox images={movieImages} />
         <Text style={styles.title}>Hello, there!</Text>
       </View>
     </>
--- services/request.js
+++ services/request.js
@@ -6,6 +6,6 @@ const apiURL = 'https://api.themoviedb.org/3';
 const apiKey = 'api_key=778306490922af732ee8ce5aeb1ef02e';
 
 export const getPopularMovies = async () => {
-  const response = await axios.get(`${apiURL}movie/popular?${apiKey}`);
+  const response = await axios.get(`${apiURL}/movie/popular?${apiKey}`);
   return response.data.results;
 };
