30e9185e5176596ce9e4f61dd0441ef05a3f1a3c
IDStack <idstack@outlook.co.id>
2022-07-29 19:24:36 +0700

    menambahkan image placeholder untuk poster yang gagal muat

--- components/CardMovie.js
+++ components/CardMovie.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
 
 import React from 'react';
-import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+const placeholderMovie = require('../assets/images/placeholder.png');
 
 const CardMovie = (props) => {
   const {movie} = props;
@@ -10,10 +12,17 @@ const CardMovie = (props) => {
       <TouchableOpacity style={styles.content}>
         <Image
           style={styles.image}
-          source={{
-            uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-          }}
+          source={
+            movie.poster_path
+              ? {
+                  uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                }
+              : placeholderMovie
+          }
         />
+        {!movie.poster_path && (
+          <Text style={styles.movieTitle}>{movie.title}</Text>
+        )}
       </TouchableOpacity>
     </View>
   );
@@ -23,11 +32,18 @@ const styles = StyleSheet.create({
   content: {
     padding: 5,
     position: 'relative',
+    alignItems: 'center',
   },
   image: {
     height: 200,
     width: 120,
     borderRadius: 20,
+  },
+  movieTitle: {
+    position: 'absolute',
+    width: 100,
+    top: 10,
+    textAlign: 'center',
   },
 });
 
