c57977009c5e48ca4fcf4ab6243f97e56f316ecd
IDStack <idstack@outlook.co.id>
2022-07-31 14:21:16 +0700

    persiapan menggunakan navigasi untuk screen

--- App.js
+++ App.js
@@ -6,14 +6,21 @@
  * @flow strict-local
  */
 
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import Home from './screens/Home';
 
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
